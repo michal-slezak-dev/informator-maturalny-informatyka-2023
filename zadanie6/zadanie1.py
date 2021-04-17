65abae0bcd0cb0dd350a7c2b74e184ffced84d07
Michał Ś <65869511+Mike920-dev@users.noreply.github.com>
2021-04-17 14:39:50 +0200

    Add files via upload

--- zadanie6/zadanie1.py
+++ zadanie6/zadanie1.py
@@ -5,12 +5,17 @@ with open("dane6.txt", "r", encoding="UTF-8") as plik:
         napisy.append(line.strip())
 # print(napisy)
 
+#Ten słownik w kluczach będzie przechowywać podstawę p a w wartościach ilość liczb p-minimalnych
 napisy_p_minimalne = {}
 
+#Wypełniamyt słownik podstawami p od 2 do 10 i wartości zerujemy
 for i in range(2, 11):
     napisy_p_minimalne.update({i: 0})
 # print(napisy_p_minimalne)
 
+# Dla każdego napisu w liście szukamy największej cyfry i dodajemy 1, żeby otrzymać podstawę p
+# Następnie dla klucza i wartości w słowniku sprawdzami, że jeśli klucz(podstawa p) jest równy tej największej cyfrze + 1 w napisie
+# To wtedy zwiększamy wartość o 1
 for napis in napisy:
     maxLiczbaSystem = int(max(napis)) + 1
 

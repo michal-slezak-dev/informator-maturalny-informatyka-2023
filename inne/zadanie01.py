513a46a42396e535c0cc442021809d185c98fa09
Michał Ś <65869511+Mike920-dev@users.noreply.github.com>
2021-04-30 14:47:06 +0200

    Add files via upload

---
+++ inne/zadanie01.py
@@ -0,0 +1,28 @@
+with open("zadanie01.txt", "r", encoding="UTF-8") as plik:
+    wiersze = []
+
+    for wiersz in plik:
+        wiersze.append(wiersz.split())
+# print(wiersze)
+
+ile = 0
+# liczba = "1231321(2)"
+# temp = liczba.split("(")
+# podstawa = temp[1].replace(")", "")
+# print(temp[0])
+
+for wiersz in wiersze:
+    temp1 = wiersz[0].split("(")
+    podstawa1 = temp1[1].replace(")", "")
+
+    temp2 = wiersz[1].split("(")
+    podstawa2 = temp2[1].replace(")", "")
+
+    liczba1 = int(temp1[0], int(podstawa1))
+    liczba2 = int(temp2[0], int(podstawa2))
+
+    if liczba1 > liczba2:
+        ile += 1
+
+print(ile)
+
---
+++ inne/zadanie02.py
@@ -0,0 +1,38 @@
+with open("zadanie02.txt", "r", encoding="UTF-8") as plik:
+    wiersze = []
+
+    for wiersz in plik:
+        wiersze.append(wiersz.split())
+# print(wiersze)
+
+ile = 0
+
+literki = ["A", 10, "B", 11, "C", 12, "D", 13, "E", 14, "F", 15]
+
+hexadecimal = dict()
+for i in range(0, 11, 2):
+    hexadecimal[literki[i]] = literki[i + 1]
+
+for wiersz in wiersze:
+    temp1 = wiersz[0].split("(")
+    podstawa1 = temp1[1].replace(")", "")
+
+    temp2 = wiersz[1].split("(")
+    podstawa2 = temp2[1].replace(")", "")
+
+    maxLiczba1 = max(temp1[0]).upper()
+    maxLiczba2 = max(temp2[0]).upper()
+
+    if maxLiczba1 in list(hexadecimal.keys()):
+        maxLiczba1 = hexadecimal[maxLiczba1]
+
+    if maxLiczba2 in list(hexadecimal.keys()):
+        maxLiczba2 = hexadecimal[maxLiczba2]
+
+    sprawdzanaPodstawa1 = int(maxLiczba1)
+    sprawdzanaPodstawa2 = int(maxLiczba2)
+
+    if sprawdzanaPodstawa1 >= int(podstawa1) or sprawdzanaPodstawa2 >= int(podstawa2):
+        ile += 1
+
+print(ile)
